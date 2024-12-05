0ea8eff6b3065351f20981da95622f7e7c7836e4
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-12-05 08:50:04 -0300

    docs: add: Configuração swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cadastro Pessoa')
+    .setDescription('API REST para cadastro de pessoas')
+    .setVersion('v1.0.0')
+    .build();
+
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, documentFactory);
+
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
