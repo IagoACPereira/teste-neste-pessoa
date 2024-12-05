ce9681b0a83a4eed13759b7f872a9797c18365e4
Iago Azevedo Costa Pereira <iago0307@gmail.com>
2024-12-04 21:05:38 -0300

    feat: add: CRUD fake

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,61 @@
-import { Controller, Get } from '@nestjs/common';
+import { 
+  Controller, 
+  Get, 
+  Post, 
+  Put, 
+  Delete, 
+  Body,
+  Param
+ } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+@Controller('cliente')
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Post()
+  adicionaPessoa(
+    @Body()
+    pessoa: {
+      nome: string,
+      idade: number,
+    }
+  ): object {
+    return this.appService.adicionaPessoa(pessoa);
+  }
+
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  pegaPessoas(): Array<object> {
+    return this.appService.pegaPessoas();
+  }
+
+  @Get(':id')
+  pegaPessoaId(
+    @Param('id')
+    id: string
+  ): object {
+    console.log(id)
+    return this.appService.pegaPessoaId(id);
+  }
+
+  @Put(':id')
+  atualizaPessoa(
+    @Param('id')
+    id: string,
+    @Body()
+    pessoa: {
+      nome: string,
+      idade: number,
+    }
+  ): object {
+    return this.appService.atualizaPessoa(id, pessoa);
+  }
+
+  @Delete(':id')
+  deletaPessoa(
+    @Param('id')
+    id: string
+  ): object {
+    return this.appService.deletaPessoa(id);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,54 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  adicionaPessoa(pessoa: {
+    nome: string,
+    idade: number
+  }): object {
+    return {
+      mensagem: 'Pessoa adicionada com sucesso',
+      pessoa: {
+        nome: pessoa.nome,
+        idade: pessoa.idade,
+      },
+      statusCode: 201,
+    };
+  }
+
+  pegaPessoas(): Array<object> {
+    return [
+      {
+        nome: 'Pessoa 1',
+        idade: 30,
+      },
+      {
+        nome: 'Pessoa 2',
+        idade: 20,
+      },
+    ];
+  }
+
+  pegaPessoaId(id: string): object {
+    return {
+      nome: 'Pessoa 1',
+      idade: 30,
+    };
+  }
+
+  atualizaPessoa(
+    id: string, 
+    pessoa: object
+  ): object {
+    return { 
+      mensagem: 'Dados atualizados com sucesso',
+      statusCode: 200
+    };
+  }
+
+  deletaPessoa(id: string): object {
+    return { 
+      mensagem: 'Dados deletados com sucesso',
+      statusCode: 200
+    };
   }
 }
